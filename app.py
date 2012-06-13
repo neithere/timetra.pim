0486619557964b4b1dd723427ccb8850279371fa
Andrey Mikhaylenko <neithere@gmail.com>
2012-06-13 18:53:42 +0600

    added day_tasks view: same as day_full but without ideas and problems, just needs and tasks (more compact).

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/<int:year>/<int:month>/<int:day>')
-def day_view(year=None, month=None, day=None):
+@app.route('/<int:year>/<int:month>/<int:day>/')
+def day_full(year=None, month=None, day=None):
     if year and month and day:
         date = datetime.date(year, month, day)
     else:
@@ -26,9 +26,22 @@ def day_view(year=None, month=None, day=None):
     return render_template('day.html', items=items, date=date, prev=prev, next=next)
 
 
+@app.route('/<int:year>/<int:month>/<int:day>/tasks/')
+def day_tasks(year=None, month=None, day=None):
+    if year and month and day:
+        date = datetime.date(year, month, day)
+    else:
+        date = datetime.date.today()
+    root = app.config['SOURCE_YAML_ROOT']
+    items = yamlfiles.get_day_plans(root, date)
+    prev = date - datetime.timedelta(days=1)
+    next = date + datetime.timedelta(days=1)
+    return render_template('day_tasks.html', items=items, date=date, prev=prev, next=next)
+
+
 if __name__ == "__main__":
     app.config.from_pyfile('conf.py')
     app.config.NAV_ITEMS = (
-        ('day_view', u'Планы'),
+        ('day_full', u'Планы'),
     )
     app.run(port=6062)
