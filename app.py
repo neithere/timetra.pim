494b0452b05dfcf97a3e0e03d1de911814bcca96
Andrey Mikhaylenko <neithere@gmail.com>
2012-06-13 21:50:38 +0600

    added support for multiple data providers (also using rstfiles from flow)
    --HG--
    rename : yamlfiles.py => providers/yamlfiles.py

--- app.py
+++ app.py
@@ -1,47 +1,72 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 import datetime
-import re
 
-from flask import Flask, render_template
-#from timetra import storage as timetra_storage
+from flask import Blueprint, Flask, current_app, render_template
 
-import yamlfiles
+from providers import yamlfiles, rstfiles
 
 
-app = Flask(__name__)
+def make_app(conf_path='conf.py'):
+    app = Flask(__name__)
+
+    app.config.from_pyfile(conf_path)
+    app.config.NAV_ITEMS = (
+        ('flare.day_full', u'Планы'),
+    )
+
+    app.register_blueprint(flare, url_prefix='/')
+
+    yaml_provider = yamlfiles.configure_provider(app)
+    rst_provider = rstfiles.configure_provider(app)
+    app.data_providers = [yaml_provider, rst_provider]
+
+    return app
 
 
-@app.route('/')
-@app.route('/<int:year>/<int:month>/<int:day>/')
+flare = Blueprint('flare', __name__)
+
+
+def collect_needs(date):
+    """ Returns a list of needs and respective tasks collected from all
+    available data providers.
+    """
+    items = []
+    for provider in current_app.data_providers:
+        items.extend(provider.get_day_plans(date))
+    return items
+
+
+@flare.route('/')
+@flare.route('<int:year>/<int:month>/<int:day>/')
 def day_full(year=None, month=None, day=None):
     if year and month and day:
         date = datetime.date(year, month, day)
     else:
         date = datetime.date.today()
-    root = app.config['SOURCE_YAML_ROOT']
-    items = yamlfiles.get_day_plans(root, date)
+    #root = app.config['SOURCE_YAML_ROOT']
+    #items = yamlfiles.get_day_plans(root, date)
+    items = collect_needs(date)
     prev = date - datetime.timedelta(days=1)
     next = date + datetime.timedelta(days=1)
     return render_template('day.html', items=items, date=date, prev=prev, next=next)
 
 
-@app.route('/<int:year>/<int:month>/<int:day>/tasks/')
+@flare.route('<int:year>/<int:month>/<int:day>/tasks/')
 def day_tasks(year=None, month=None, day=None):
     if year and month and day:
         date = datetime.date(year, month, day)
     else:
         date = datetime.date.today()
-    root = app.config['SOURCE_YAML_ROOT']
-    items = yamlfiles.get_day_plans(root, date)
+    items = collect_needs(date)
+    #items = []
+    #for provider in app.data_providers:
+    #    items.extend(provider.get_day_plans(date))
     prev = date - datetime.timedelta(days=1)
     next = date + datetime.timedelta(days=1)
     return render_template('day_tasks.html', items=items, date=date, prev=prev, next=next)
 
 
 if __name__ == "__main__":
-    app.config.from_pyfile('conf.py')
-    app.config.NAV_ITEMS = (
-        ('day_full', u'Планы'),
-    )
+    app = make_app()
     app.run(port=6062)
--- providers/yamlfiles.py
+++ providers/yamlfiles.py
@@ -21,3 +21,29 @@ def get_day_plans(root_dir, date=None):
     today_path = os.path.join(day_dir, today_fn)
     with codecs.open(today_path, encoding='utf-8') as f:
         return yaml.load(f.read())
+
+
+def capfirst(value):
+    return value[0].upper() + value[1:] if value else value
+
+
+class YAMLFilesProvider:
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+
+    @staticmethod
+    def _transform_item(item):
+        item = dict(item)
+        for key in ('idea', 'note', 'risk', 'need'):
+            if item.get(key):
+                item[key] = capfirst(item[key])
+        return item
+
+    def get_day_plans(self, date):
+        items = get_day_plans(self.root_dir, date)
+        return [self._transform_item(x) for x in items]
+
+
+def configure_provider(app):
+    root_dir = app.config['SOURCE_YAML_ROOT']
+    return YAMLFilesProvider(root_dir)
