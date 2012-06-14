e73b8efe4d8fcd5679bf6453512102344f1cd4eb
Andrey Mikhaylenko <neithere@gmail.com>
2012-06-14 17:21:37 +0600

    Refactoring: moved pim-flare stuff to a module/blueprint and separated from the core app for easier further integration with pim-flow.
    --HG--
    rename : app.py => flare/__init__.py
    rename : providers/__init__.py => flare/providers/__init__.py
    rename : providers/yamlfiles.py => flare/providers/yamlfiles.py
    rename : templates/_helpers.html => flare/templates/_helpers.html
    rename : templates/base.html => flare/templates/base.html
    rename : templates/day.html => flare/templates/day.html
    rename : templates/day_notes.html => flare/templates/day_notes.html
    rename : templates/day_tasks.html => flare/templates/day_tasks.html

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 import datetime
 
-from flask import Blueprint, Flask, current_app, render_template
+from flask import Flask
 
-from providers import yamlfiles, rstfiles
+from flare import flare
+from flare.providers import yamlfiles, rstfiles
 
 
 def make_app(conf_path='conf.py'):
@@ -24,47 +25,6 @@ def make_app(conf_path='conf.py'):
     app.jinja_env.globals['now'] = datetime.datetime.now
 
     return app
-
-
-flare = Blueprint('flare', __name__)
-
-
-def collect_needs(date):
-    """ Returns a list of needs and respective tasks collected from all
-    available data providers.
-    """
-    items = []
-    for provider in current_app.data_providers:
-        items.extend(provider.get_day_plans(date))
-    return items
-
-
-def day_view(year=None, month=None, day=None, template=None):
-    assert template
-    if year and month and day:
-        date = datetime.date(year, month, day)
-    else:
-        date = datetime.date.today()
-    items = collect_needs(date)
-    prev = date - datetime.timedelta(days=1)
-    next = date + datetime.timedelta(days=1)
-    return render_template(template, items=items, date=date, prev=prev, next=next)
-
-
-@flare.route('/')
-@flare.route('<int:year>/<int:month>/<int:day>/')
-def day_full(**kwargs):
-    return day_view(template='day.html', **kwargs)
-
-
-@flare.route('<int:year>/<int:month>/<int:day>/tasks/')
-def day_tasks(**kwargs):
-    return day_view(template='day_tasks.html', **kwargs)
-
-
-@flare.route('<int:year>/<int:month>/<int:day>/notes/')
-def day_notes(**kwargs):
-    return day_view(template='day_notes.html', **kwargs)
 
 
 if __name__ == "__main__":
